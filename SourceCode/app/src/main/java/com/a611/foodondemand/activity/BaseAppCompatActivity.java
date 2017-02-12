04be5a6265466095edb1c60faf0655fa24d8deeb
dkvu <dkvu.cntt@gmail.com>
2017-02-12 19:10:53 +0700

    fix title remove back button

--- SourceCode/app/src/main/java/com/a611/foodondemand/activity/BaseAppCompatActivity.java
+++ SourceCode/app/src/main/java/com/a611/foodondemand/activity/BaseAppCompatActivity.java
@@ -2,6 +2,7 @@ package com.a611.foodondemand.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by pnhung on 2/11/2017.
@@ -9,13 +10,21 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseAppCompatActivity extends AppCompatActivity {
 
+    protected Toolbar toolbar;
+
     protected abstract int getContainerId();
     protected  abstract void findViewById();
+    protected  abstract String titleName();
+
+    protected  void changeTitleActivity(Toolbar toolbar) {
+        toolbar.setTitle(titleName());
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(getContainerId());
         findViewById();
+        changeTitleActivity(toolbar);
     }
 }
